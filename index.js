f5057fc68c17b4849b472a94b5586e6b1816bc7a
--replace-all <jonathonsmcclendon@gmail.com>
2021-08-09 13:41:17 -0500

    commit tree 6e2a35cafea22f8968be4839044d4680391bfb99

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    changeElement("text", "This is really cool!")
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  function changeElement(where, what){
+    location = document.getElementById(where).innerHTML = what
+  }
